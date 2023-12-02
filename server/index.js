e5f6521f9b3c8224d805b6c56fc41bdadce10435
ntlong1801 <ntlongcbl@gmail.com>
2023-12-02 15:49:56 +0700

    turn off cors policy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const router = require("./routes");
+const cors = require("cors");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = require("./config/swaggerConfig");
 require("dotenv").config();
+
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -16,6 +18,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(cors());
 
 router(app);
 
