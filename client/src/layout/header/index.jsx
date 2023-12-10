f4d5de155431624a63bd17cb82aa4ebbfe429674
ntlong1801 <ntlongcbl@gmail.com>
2023-12-10 16:57:51 +0700

    add i18next

--- client/src/layout/header/index.jsx
+++ client/src/layout/header/index.jsx
@@ -5,12 +5,19 @@ import { Link, useNavigate } from 'react-router-dom';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { useRef } from 'react';
 import instance from 'config';
+import { useTranslation } from 'react-i18next';
+import LanguageSelect from 'components/LanguageSelect';
 
 export default function Header() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const profileRef = useRef(null);
+  const settingRef = useRef(null);
   const user = JSON.parse(localStorage.getItem('user_profile'));
 
+  const showSettingModal = (event) => {
+    settingRef.current.toggle(event);
+  };
   const showProfileModal = (event) => {
     profileRef.current.toggle(event);
   };
@@ -25,10 +32,10 @@ export default function Header() {
     return '/dashboard';
   };
   return (
-    <div className="flex justify-content-between p-2 bg-primary sticky top-0" style={{ height: '10vh' }}>
+    <div className="flex justify-content-between p-2 bg-header sticky top-0" style={{ height: '10vh' }}>
       <div className="flex align-items-center p-2 ml-2">
         <Link to={handleGoHome()}>
-          <i className="pi pi-home text-purple-900" style={{ fontSize: '2rem' }}>Home</i>
+          <i className="pi pi-home text-white" style={{ fontSize: '2rem' }}>{t('home')}</i>
         </Link>
       </div>
       <div className="flex gap-2 align-items-center">
@@ -36,61 +43,90 @@ export default function Header() {
           <>
             <Link to="/signin">
               <Button
-                label="Sign in"
-                severity="help"
+                label={t('signIn.name')}
+                severity="primary"
                 type="button"
+
               />
             </Link>
             <Link to="/signup">
               <Button
-                label="Sign up"
+                label={t('signUp.name')}
                 severity="help"
                 type="button"
               />
             </Link>
-          </>
-        ) : (
-          <Button icon="pi pi-user" severity="help" aria-label="User" rounded onClick={showProfileModal}>
-            {/* <i className="pi pi-user mr-2" style={{ fontSize: '2rem' }} /> */}
+            <button type="button" className="p-link layout-topbar-button mr-2" onClick={showSettingModal}>
+              <i className="pi pi-cog text-white" style={{ fontSize: '1.5rem' }} />
+              {/* <span>{t('settings.name')}</span> */}
+            </button>
             <OverlayPanel
-              ref={profileRef}
+              ref={settingRef}
               appendTo={typeof window !== 'undefined' ? document.body : null}
               showCloseIcon={false}
-              id="overlay_panel_profile"
+              id="overlay_panel_setting"
+              style={{ width: '210px' }}
             >
-              <div className="flex align-items-center p-3">
-                <i className="pi pi-user text-2xl" />
-                <div className="ml-4">
-                  <h6 className="m-0">{user.email}</h6>
-                  <p>
-                    {user.full_name}
-                  </p>
-                </div>
-              </div>
-              <hr className="my-2" />
-              <ul className="profile-menu list-none p-0 m-0">
-
-                <Link to="/me" className="text-color">
-                  <li className="hover:surface-200 p-2">
-                    <i className="pi pi-user mr-3" />
-                    Profile
-                  </li>
-                </Link>
-
-                <hr />
-
-                <li className="hover:surface-200 p-2 span-button">
-                  <div onClick={handleLogout}>
-                    <span>
-                      <i className="pi pi-sign-out mr-3" />
-                      Logout
-                    </span>
-                  </div>
-                </li>
-
-              </ul>
+              <LanguageSelect />
             </OverlayPanel>
-          </Button>
+          </>
+        ) : (
+          <>
+            <Button icon="pi pi-user" severity="help" aria-label="User" rounded onClick={showProfileModal}>
+              {/* <i className="pi pi-user mr-2" style={{ fontSize: '2rem' }} /> */}
+              <OverlayPanel
+                ref={profileRef}
+                appendTo={typeof window !== 'undefined' ? document.body : null}
+                showCloseIcon={false}
+                id="overlay_panel_profile"
+              >
+                <div className="flex align-items-center p-3">
+                  <i className="pi pi-user text-2xl" />
+                  <div className="ml-4">
+                    <h6 className="m-0">{user.email}</h6>
+                    <p>
+                      {user.full_name}
+                    </p>
+                  </div>
+                </div>
+                <hr className="my-2" />
+                <ul className="profile-menu list-none p-0 m-0">
+
+                  <Link to="/me" className="text-color">
+                    <li className="hover:surface-200 p-2">
+                      <i className="pi pi-user mr-3" />
+                      {t('settings.profile')}
+                    </li>
+                  </Link>
+
+                  <hr />
+                  <div onClick={handleLogout}>
+                    <li className="hover:surface-200 p-2 span-button">
+
+                      <span>
+                        <i className="pi pi-sign-out mr-3" />
+                        {t('settings.logout')}
+                      </span>
+                    </li>
+                  </div>
+
+                </ul>
+              </OverlayPanel>
+            </Button>
+            <button type="button" className="p-link layout-topbar-button mr-2" onClick={showSettingModal}>
+              <i className="pi pi-cog text-white" style={{ fontSize: '1.5rem' }} />
+              {/* <span>{t('settings.name')}</span> */}
+            </button>
+            <OverlayPanel
+              ref={settingRef}
+              appendTo={typeof window !== 'undefined' ? document.body : null}
+              showCloseIcon={false}
+              id="overlay_panel_setting"
+              style={{ width: '210px' }}
+            >
+              <LanguageSelect />
+            </OverlayPanel>
+          </>
         )}
       </div>
     </div>
