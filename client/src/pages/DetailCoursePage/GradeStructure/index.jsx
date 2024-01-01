c9658fc0e6a48c1873fc3aa57fdafe6d992810d9
caoqui <qui7d18@gmail.com>
2024-01-01 13:24:12 +0700

    add Arrange a grade composition position

--- client/src/pages/DetailCoursePage/GradeStructure/index.jsx
+++ client/src/pages/DetailCoursePage/GradeStructure/index.jsx
@@ -1,9 +1,15 @@
 import { useMutation, useQuery } from 'react-query';
-import { getGradeStructure, editGradeStructure, isTeacherOfClass, getClassByID } from 'apis/class.api';
+import {
+  getGradeStructure,
+  editGradeStructure,
+  isTeacherOfClass,
+  getClassByID,
+} from 'apis/class.api';
 
 import React, { useState, useMemo, useEffect } from 'react';
 import { useParams } from 'react-router';
 import './style.scss';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const user = JSON.parse(localStorage.getItem('user_profile'));
 
@@ -18,12 +24,12 @@ function GradeStructure() {
 
   const { data: _data } = useQuery({
     queryKey: ['class', classId],
-    queryFn: () => getClassByID(classId)
+    queryFn: () => getClassByID(classId),
   });
   useMemo(() => _data?.data ?? [], [_data]);
   const { data: checkTeacher } = useQuery({
     queryKey: [classId],
-    queryFn: () => isTeacherOfClass(user?.id, classId)
+    queryFn: () => isTeacherOfClass(user?.id, classId),
   });
   const isTeacher = useMemo(() => checkTeacher?.data?.status !== 'false', [checkTeacher]);
 
@@ -80,68 +86,95 @@ function GradeStructure() {
     setIsFormVisible(false);
   };
   return (
-    <div className="grade-structure">
-      <button type="button" onClick={showForm}>
-        Grade Structure
-      </button>
+    <DragDropContext>
+      <div className="grade-structure">
+        <button type="button" onClick={showForm}>
+          Grade Structure
+        </button>
 
-      {isFormVisible && (
-        <div className="overlay">
-          <div className="form-container">
-            {listGrade.map((grade, index) => (
-              <form>
-                <div className="form-group">
-                  <label htmlFor="componentName">Tên:</label>
-                  <input
-                    type="text"
-                    id="componentName"
-                    name="componentName"
-                    value={grade.name}
-                    onChange={(e) => handleChangeValue(index, 'name', e.target.value)}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="percentage">%:</label>
-                  <input
-                    type="text"
-                    id="percentage"
-                    name="percentage"
-                    maxLength="5"
-                    value={grade.grade_scale}
-                    onChange={(e) => handleChangeValue(index, 'grade_scale', e.target.value)}
-                  />
-                  {isTeacherToEdit && (
-                    <button
-                      type="button"
-                      className="delete-button"
-                      onClick={() => handleDeleteGrade(index)}
-                    >
-                      x
-                    </button>
-                  )}
-                </div>
-              </form>
-            ))}
-            {isTeacherToEdit && (
-              <button type="button" className="add-button" onClick={() => handleAddNewGrade()}>
-                +
-              </button>
-            )}
+        {isFormVisible && (
+          <div className="overlay">
+            <div className="form-container">
+              <Droppable droppableId="grades">
+                {(provided) => (
+                  <div
+                    /* eslint-disable-next-line react/jsx-props-no-spreading */
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {listGrade.map((grade, index) => (
+                      <Draggable draggableId={`grade-${index}`} index={index}>
+                        {(provided2) => (
+                          <div
+                            ref={provided2.innerRef}
+                            /* eslint-disable-next-line react/jsx-props-no-spreading */
+                            {...provided2.draggableProps}
+                            /* eslint-disable-next-line react/jsx-props-no-spreading */
+                            {...provided2.dragHandleProps}
+                          >
+                            <form>
+                              <div className="form-group">
+                                <label htmlFor="componentName">Tên:</label>
+                                <input
+                                  type="text"
+                                  id="componentName"
+                                  name="componentName"
+                                  value={grade.name}
+                                  onChange={(e) => handleChangeValue(index, 'name', e.target.value)}
+                                />
+                              </div>
+                              <div className="form-group">
+                                <label htmlFor="percentage">%:</label>
+                                <input
+                                  type="text"
+                                  id="percentage"
+                                  name="percentage"
+                                  maxLength="5"
+                                  value={grade.grade_scale}
+                                  onChange={(e) =>
+                                    handleChangeValue(index, 'grade_scale', e.target.value)}
+                                />
+                                {isTeacherToEdit && (
+                                  <button
+                                    type="button"
+                                    className="delete-button"
+                                    onClick={() => handleDeleteGrade(index)}
+                                  >
+                                    x
+                                  </button>
+                                )}
+                              </div>
+                            </form>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
 
-            <div className="form-buttons">
-              <button type="button" className="close-button" onClick={hideForm}>
-                Close
-              </button>
               {isTeacherToEdit && (
-                <button type="button" className="submit-button" onClick={handleSave}>
-                  Save
+                <button type="button" className="add-button" onClick={handleAddNewGrade}>
+                  +
                 </button>
               )}
+
+              <div className="form-buttons">
+                <button type="button" className="close-button" onClick={hideForm}>
+                  Close
+                </button>
+                {isTeacherToEdit && (
+                  <button type="button" className="submit-button" onClick={handleSave}>
+                    Save
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </DragDropContext>
   );
 }
 
